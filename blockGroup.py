45f7db3b7e6e6b7eb3eeeab719f23fb5e1bf2a22
Hanson <1198491837@qq.com>
2024-04-13 11:10:56 +0800

    first commit

---
+++ blockGroup.py
@@ -0,0 +1,194 @@
+import random
+import pygame.mixer
+from block import *
+from utils import *
+
+import const
+
+class BlockGroup(object):
+    def GenerateBlockGroupConfig(rowIdx, colIdx, shapeIdx = -1):
+        if shapeIdx == -1:
+            shapeIdx = random.randint(0, len(const.BLOCK_SHAPES)-1)
+        bType = random.randint(0, const.BlockType.BLOCKMAX-1)
+        configList = []
+        rotateIdx = 0
+        for i in range(len(const.BLOCK_SHAPES[shapeIdx][rotateIdx])):
+            config = {
+                'blockType': bType,
+                'blockShape': shapeIdx,
+                'blockRotate': rotateIdx,
+                'blockGroupIdx': i,
+                'rowIdx': rowIdx,
+                'colIdx': colIdx
+            }
+            configList.append(config)
+        return configList
+
+    def __init__(self, blockGroupType, width, height, blockConfigList, relPos):
+        super().__init__()
+        self.blocks = []
+        self.blockGroupType = blockGroupType
+        self.dropTime = getCurrentTime()
+        self.dropInterval = 900
+        self.pressTime = {}
+        self.eliminate = False
+        self.eliminateRow = []
+        self.eliminateTime = 0
+        for config in blockConfigList:
+            block = Block(config['blockType'], config['rowIdx'], config['colIdx'], config['blockShape'],
+                        config['blockRotate'], config['blockGroupIdx'], width, height, relPos)
+            self.blocks.append(block)
+        self.isPause = False
+        self.isFallingDown = False
+
+    def setDropInterval(self, dropInterval):
+        self.dropInterval = dropInterval
+
+    def getDropInterval(self):
+        return self.dropInterval
+
+    def getFallingDown(self):
+        return self.isFallingDown
+    
+    def setFallingDown(self, isFallingDown):
+        self.isFallingDown = isFallingDown
+        if isFallingDown:
+            pygame.key.set_mods(KMOD_MODE)
+        else:
+            pygame.key.set_mods(0)
+
+    def setBaseIndexes(self, baseRow, baseCol):
+        for block in self.blocks:
+            block.setBaseIndex(baseRow, baseCol)
+
+    def getBlockIndexes(self):
+        return [block.getIndex() for block in self.blocks]
+
+    def getNextBlockIndexes(self):
+        return [block.getNextIndex() for block in self.blocks]
+
+    def draw(self, surface):
+        for b in self.blocks:
+            b.draw(surface)
+
+    def getBlocks(self):
+        return self.blocks
+
+    def clearBlocks(self):
+        self.blocks = []
+
+    def addBlocks(self, block):
+        self.blocks.append(block)
+
+    def checkAndSetPressTime(self, key):
+        ret = False
+        if getCurrentTime() - self.pressTime.get(key, 0) > 150:
+            ret = True
+            self.pressTime[key] = getCurrentTime()
+        return ret
+
+    def keyDownHandler(self):
+        pressed = pygame.key.get_pressed()
+        if pressed[K_LEFT] and self.checkAndSetPressTime(K_LEFT) and not self.isPause:
+            b = True
+            for block in self.blocks:
+                if block.isLeftBoundary():
+                    b = False
+                    break
+            if b:
+                for block in self.blocks:
+                    block.moveLeft()
+
+        if pressed[K_RIGHT] and self.checkAndSetPressTime(K_RIGHT) and not self.isPause:
+            b = True
+            for block in self.blocks:
+                if block.isRightBoundary():
+                    b = False
+                    break
+            if b:
+                for block in self.blocks:
+                    block.moveRight()
+
+        if pressed[K_UP] and self.checkAndSetPressTime(K_UP) and not self.isPause:
+            for block in self.blocks:
+                block.rotate()
+
+        if pressed[K_DOWN] and not self.isPause:
+            self.setFallingDown(True)
+
+        if self.blockGroupType == const.BlockGroupType.DROP:
+            for block in self.blocks:
+                block.setShadow(pressed[K_DOWN])
+
+    def update(self):
+        oldTime = self.dropTime
+        curTime = getCurrentTime()
+        diffTime = curTime - oldTime
+        if self.blockGroupType == const.BlockGroupType.DROP:
+            if diffTime >= self.dropInterval:
+                self.dropTime = curTime
+                for b in self.blocks:
+                    b.drop(1)
+            self.keyDownHandler()
+            if self.getFallingDown():
+                self.setDropInterval(30)
+
+        for block in self.blocks:
+            block.update()
+
+        if self.getEliminate():
+            if getCurrentTime() - self.eliminateTime > 500:
+                tmpBlocks = []
+                for block in self.blocks:
+                    if block.getIndex()[0] not in self.eliminateRow:
+                        if block.getIndex()[0] < self.eliminateRow[0]:
+                            block.drop(len(self.eliminateRow))
+                        tmpBlocks.append(block)
+                self.eliminateRow.clear()
+                self.blocks = tmpBlocks
+                self.setEliminate(False)
+
+    def doEliminate(self, row):
+        eliminateRow = {}
+        for col in range(0, const.GAME_COL):
+            idx = (row, col)
+            eliminateRow[idx] = 1
+        self.setEliminate(True)
+        self.eliminateRow.append(row)
+
+        for block in self.blocks:
+            if eliminateRow.get(block.getIndex()):
+                block.startBlink()
+        sound = pygame.mixer.Sound(const.ELIMINATE_SOUND)
+        sound.set_volume(0.2)
+        sound.play()
+
+    def processEliminate(self):
+        hash = {}
+
+        allIndexes = self.getBlockIndexes()
+        for idx in allIndexes:
+            hash[idx] = 1
+
+        eliminateRows = []
+        for row in range(const.GAME_ROW - 1, -1, -1):
+            full = True
+            for col in range(0, const.GAME_COL):
+                idx = (row, col)
+                if not hash.get(idx):
+                    full = False
+                    break
+            if full:
+                eliminateRows.append(row)
+
+        if eliminateRows:
+            for row in eliminateRows:
+                self.doEliminate(row)
+        return len(eliminateRows)
+
+    def setEliminate(self, bEl):
+        self.eliminate = bEl
+        self.eliminateTime = getCurrentTime()
+
+    def getEliminate(self):
+        return self.eliminate
---
+++ game.py
@@ -0,0 +1,122 @@
+from blockGroup import *
+import const
+
+class Game(pygame.sprite.Sprite):
+    def getRelPos(self):
+        return (240, 50)
+
+    def __init__(self, screen):
+        self.font = pygame.font.Font(None, 60)
+        self.score = 0
+        self.screen = screen
+        self.fixedBlockGroup = BlockGroup(const.BlockGroupType.FIXED, const.BLOCK_SIZE_W, const.BLOCK_SIZE_H, [], self.getRelPos())
+        self.dropBlockGroup = None
+        self.nextBlockGroup = None
+        self.generateNextBlockGroup()
+        self.gameOverImage = pygame.image.load("pic/gameover.png")
+        self.isGameOver = False
+        self.isPlayGameOverSound = False
+        self.isPause = False
+        self.pause_button_image = pygame.image.load(const.PAUSE_BUTTON_IMAGE)
+        self.restart_button_image = pygame.image.load(const.RESTART_BUTTON_IMAGE)
+
+    def getPause(self):
+        return self.isPause
+    
+    def setPause(self, isPause):
+        self.isPause = isPause
+
+    def getGameOver(self):
+        return self.isGameOver
+
+    def generateDropBlockGroup(self):
+        self.dropBlockGroup = self.nextBlockGroup
+        self.dropBlockGroup.setBaseIndexes(0, const.GAME_COL/2-1)
+        self.generateNextBlockGroup()
+    
+    def generateNextBlockGroup(self):
+        conf = BlockGroup.GenerateBlockGroupConfig(0, const.GAME_COL + 3)
+        self.nextBlockGroup = BlockGroup(const.BlockGroupType.DROP, const.BLOCK_SIZE_W, const.BLOCK_SIZE_H, conf, self.getRelPos())
+
+    def isCollision(self):
+        hash = {}
+        allIndexes = self.fixedBlockGroup.getBlockIndexes()
+        for idx in allIndexes:
+            hash[idx] = 1
+        dropIndexes = self.dropBlockGroup.getNextBlockIndexes()
+
+        for dropIndex in dropIndexes:
+            if hash.get(dropIndex):
+                return True
+            if dropIndex[0] >= const.GAME_ROW:
+                return True
+        return False
+
+    def update(self):
+        if self.isGameOver:
+            return
+        self.checkGameOver()
+
+        self.fixedBlockGroup.update()
+
+        if self.fixedBlockGroup.getEliminate():
+            return
+
+        if self.dropBlockGroup:
+            self.dropBlockGroup.update()
+        else:
+            self.generateDropBlockGroup()
+            self.dropBlockGroup.setDropInterval(900)
+
+        if self.isCollision():
+            self.dropBlockGroup.setFallingDown(False)
+            blocks = self.dropBlockGroup.getBlocks()
+            for block in blocks:
+                self.fixedBlockGroup.addBlocks(block)
+            self.dropBlockGroup.clearBlocks()
+            self.dropBlockGroup = None
+            eliminateRows = self.fixedBlockGroup.processEliminate()
+            if eliminateRows > 0:
+                self.score += 1 * eliminateRows
+
+    def checkGameOver(self):
+        allIndexes = self.fixedBlockGroup.getBlockIndexes()
+        for index in allIndexes:
+            if index[0] < 2:
+                self.isGameOver = True
+
+    def draw(self):
+        self.fixedBlockGroup.draw(self.screen)
+        if self.dropBlockGroup:
+            self.dropBlockGroup.draw(self.screen)
+        self.nextBlockGroup.draw(self.screen)
+
+        textImage = self.font.render('Score: ' + str(self.score), True, (255,255,255))
+        self.screen.blit(textImage, (10, 80))
+        if self.isGameOver:
+            rect = self.gameOverImage.get_rect()
+            rect.centerx = const.GAME_WIDTH_SIZE / 2
+            rect.centery = const.GAME_HEIGHT_SIZE / 2
+            self.screen.blit(self.gameOverImage, rect)
+            if not self.isPlayGameOverSound:
+                sound = pygame.mixer.Sound(const.GAMEOVER_SOUND)
+                sound.play()
+                while pygame.mixer.get_busy():
+                    pass
+                self.isPlayGameOverSound = True
+        
+    def drawPauseSurface(self):
+        pause_text = pygame.font.SysFont(None, 40).render("Game Paused", True, (255, 0, 0))
+        pause_hint = pygame.font.SysFont(None, 40).render("Press ESC or the Pause to continue", True, (255, 255, 255))
+        self.screen.blit(pause_text, (const.GAME_WIDTH_SIZE // 2 - pause_text.get_width() // 2,
+                                      const.GAME_HEIGHT_SIZE // 2 - pause_text.get_height() // 2 - 30))
+        self.screen.blit(pause_hint, (const.GAME_WIDTH_SIZE // 2 - pause_text.get_width() // 2 - 125,
+                                      const.GAME_HEIGHT_SIZE // 2 - pause_text.get_height() // 2))
+        
+    def restart(self):
+        self.fixedBlockGroup.clearBlocks()
+        self.score = 0
+        self.isGameOver = False
+        self.isPlayGameOverSound = False
+        self.generateDropBlockGroup()
+        self.generateNextBlockGroup()
